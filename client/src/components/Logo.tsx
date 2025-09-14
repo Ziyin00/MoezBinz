78a4e76c399063a21c778424b9f777720d4a8a76
Ziyin Ab <ziyinab00@gmail.com>
2025-09-14 21:36:42 +0300

    logo icon add

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 
 const Logo: React.FC = () => {
   return (
-    <div className="text-2xl font-bold tracking-wider">
-      <span className="text-red-600">MOEZ</span>
-      <span className="text-gray-900"> BINZ</span>
-      {/* <img src="/logo.png" alt="Logo" width={100} height={100} />  */}
+    <div className="flex items-center">
+      <img src="/logo.jpg" alt="Moez Binz Logo" className="h-10 w-auto mr-3" />
+      <div className="text-2xl font-bold tracking-wider">
+        <span className="text-red-600">MOEZ</span>
+        <span className="text-gray-900"> BINZ</span>
+      </div>
     </div>
   );
 };
